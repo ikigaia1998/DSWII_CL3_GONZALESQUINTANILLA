9c223f520d553e3db8426ca1d23c70b9acf40c0d
ikigaia1998 <i202117849@cibertec.edu.pe>
2023-11-25 21:20:27 -0500

    Initial commit

---
+++ src/main/java/com/dswii_cl3_gonzalesquintanilla/DswiiCl3GonzalesquintanillaApplication.java
@@ -0,0 +1,13 @@
+package com.dswii_cl3_gonzalesquintanilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiCl3GonzalesquintanillaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DswiiCl3GonzalesquintanillaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dswii_cl3_gonzalesquintanilla/DswiiCl3GonzalesquintanillaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dswii_cl3_gonzalesquintanilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiCl3GonzalesquintanillaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
